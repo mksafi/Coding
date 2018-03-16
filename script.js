67bc07791b00ec23dbfa9478cea1caaf92ddf922
Unknown <mk.safi@live.com>
2018-03-15 22:53:49 -0700

    js file added

---
+++ script.js
@@ -0,0 +1,12 @@
+function addNumbers(a, b) {
+    let c = a + b;
+}
+function totalPrice(sqEdge, pricePerSqFe) {
+    return(sqEdge * pricePerSqFe);
+}
+let currentTime = 0600;
+if (currentTime === 0600) {
+    alert("Wake Up!");
+} else {
+    alert("Not Time To Wake Up!");
+}
